c57a2fb13484d47957ca2f3374bea37a268e63ce
Logan Monson <scarletfedora@gmail.com>
2022-07-09 15:01:42 -0700

    editting the client crud routes, figuring out sw

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,34 +1,52 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     devServer: {
-      hot: 'only',
+      hot: "only",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Webpack Plugin'
+        template: "./index.html",
+        title: "Webpack Plugin",
       }),
       //
       new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'service-worker.js'
+        swSrc: "./src-sw.js",
+        swDest: "service-worker.js",
+      }),
+
+      new WebpackPwaManifest({
+        filename: "manifest.json",
+        name: "Just Another Text Editor",
+        shortname: "JATE",
+        description: "blamk for now",
+        background_color: "#fef292",
+        theme_color: "#fef292",
+        crossorigin: "use-credentials",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [32, 64, 96, 128, 256, 512],
+          },
+        ],
       }),
       //
     ],
@@ -37,21 +55,21 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
+          type: "asset/resource",
         },
         {
           test: /\.m?js$/,
           exclude: /(node_modules|bower_components)/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env']
-            }
-          }
+              presets: ["@babel/preset-env"],
+            },
+          },
         },
       ],
     },
